d80f8728ca6fa925a876ea52303b2d7e8e820e2d
Maciek Kucmus <maciejkucmus@gmail.com>
2023-05-23 12:17:19 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,20 @@
-import PostCSSOptions from "./postcss.config";
-import { resolve, dirname } from "node:path";
-import { fileURLToPath } from "url";
-import VueI18nVitePlugin from "@intlify/unplugin-vue-i18n/vite";
+import PostCSSOptions from './postcss.config';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite';
 
 export default defineNuxtConfig({
-  modules: ["@vueuse/nuxt", "@shopware-pwa/nuxt3-module"],
-  css: ["@/assets/css/main.css"],
+  modules: ['@vueuse/nuxt', '@shopware-pwa/nuxt3-module'],
+  css: ['@/assets/css/main.css'],
+  runtimeConfig: {
+    public: {
+      shopware: {
+        shopwareEndpoint: 'https://demo-frontends.shopware.store',
+        shopwareAccessToken: 'SWSCBHFSNTVMAWNZDNFKSHLAYW',
+        devStorefrontUrl: '',
+      },
+    },
+  },
   build: {
     postcss: {
       postcssOptions: PostCSSOptions,
@@ -15,7 +24,7 @@ export default defineNuxtConfig({
     plugins: [
       VueI18nVitePlugin({
         include: [
-          resolve(dirname(fileURLToPath(import.meta.url)), "./locales/*.json"),
+          resolve(dirname(fileURLToPath(import.meta.url)), './locales/*.json'),
         ],
       }),
     ],
